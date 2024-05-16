09a9f3e6415490aa7625f8dd05c406817c6bef93
AntoineTSIO <antoine.touzart@outlook.com>
2024-05-17 00:15:30 +0200

    feat: access to stations list from database in app and deploy it on ios

--- lib/map_page/map_locations.dart
+++ lib/map_page/map_locations.dart
@@ -1,24 +1,35 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 Future<List<String>> fetchDepartureLocations() async {
-  await Future.delayed(const Duration(seconds: 2));
-  return [
-    'Paris',
-    'Lyon',
-    'Marseille',
-    'Nice',
-    'Tokyo',
-    'Kyoto',
-    'Osaka',
-  ];
+  final List<Map<String, String>> stations = await fetchStationList();
+  return stations.map((station) => station['nom']!).toList();
 }
 
 Future<List<String>> fetchArrivalLocations() async {
-  await Future.delayed(const Duration(seconds: 2));
-  return [
-    'New York',
-    'Los Angeles',
-    'Chicago',
-    'San Francisco',
-  ];
+  final List<Map<String, String>> stations = await fetchStationList();
+  return stations.map((station) => station['nom']!).toList();
+}
+
+Future<String?> fetchStationsData() async {
+  String apiUrl = 'http://192.168.0.202:8000/request/stations';
+  try {
+    http.Response response = await http.get(Uri.parse(apiUrl));
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      print('Erreur: ${response.statusCode}');
+      return null;
+    }
+  } catch (e) {
+    print('Erreur de connexion: $e');
+    return null;
+  }
+}
+
+Future<List<Map<String, String>>> fetchStationList() async {
+  final String? jsonData = await fetchStationsData();
+  final List<dynamic> data = json.decode(jsonData!)['stations'];
+  return data.map((station) => Map<String, String>.from(station)).toList();
 }
--- lib/map_page/map_page.dart
+++ lib/map_page/map_page.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'map_locations.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
